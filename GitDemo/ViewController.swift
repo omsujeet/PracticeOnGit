1ec72efda13b62fef48add935f1ca97d6e22f707
a1u02gur <a_sujeet1.singh@airtel.com>
2018-02-04 14:34:18 +0530

    commit 2

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var img : UIImageView!
-
+    @IBOutlet weak var viewContainer : UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
